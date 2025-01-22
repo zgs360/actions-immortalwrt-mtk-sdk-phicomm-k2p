6abf3bb550bfca1a56134644635497fc5f37763f
zgs360 <zgs360@gmail.com>
2025-01-22 20:50:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,12 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # Modify default Hostname
 sed -i 's/ImmortalWrt/K2P/g' package/base-files/files/bin/config_generate
 
+# 设置root密码为root
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./root:$1$NHm9Sd0k$3.SLfxcsFdbFxofPDldWd0:20109:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+# 最大连接数修改为65535
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
 #Enable 802.11k/v/r
 #sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
 #sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
